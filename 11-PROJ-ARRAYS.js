081867d1a7d1143df6928e6133f075870d691c24
DARKVIEWNERD <vincentnarvaez391@gmail.com>
2024-08-11 17:25:00 +0800

    solve misused function

--- 11-PROJ-ARRAYS.js
+++ 11-PROJ-ARRAYS.js
@@ -1,25 +1,34 @@
 const inputElement=[];
 
+function render(){
+    let htmlLists='';
+    for(let count=0;count<inputElement.length;count++){
+    
+        const valueobject=inputElement[count];// using variable inside the bracket well help access value inside an array     
+        const{name,duedate}=valueobject;
+         const html= 
+         `<p>${name}${duedate}<button
+         onclick="inputElement.splice(${count},1);render();"
+         >Delete</button></p>`;//error misused of function that's why there bug like stay button etc.
+         htmlLists+=html;
+    
+    }
+    document.querySelector('.js-todo-list').innerHTML= htmlLists;
+    console.log(htmlLists);
+}
 function addtodo(){
    
+ 
    const todoList= document.querySelector('.js-todo-name').value;
    const todoDate= document.querySelector('.js-todo-date').value;
    
-   inputElement.push( `${todoList} ${todoDate}`);
-   console.log(inputElement.length);
-   let htmlLists='';
-   document.querySelector('.js-todo-name').value='';
-for(let count=0;count<inputElement.length;count++){
-    
-    const value=inputElement[count];// using variable inside the bracket well help access value inside an array
-     const html= `<p>${value}</p><button>Delete</button>`;
-     htmlLists+=html;
+   inputElement.push( {name: todoList,duedate: todoDate});
+  // console.log(inputElement.length);
+  
 
-}
-    console.log(htmlLists);
-    document.querySelector('.js-todo-list').innerHTML= htmlLists;
-    
-
+    document.querySelector('.js-todo-name').value='';
+   // document.querySelector('.js-todo-date').value='';
+    render();
 }
 const inputElement2=[];
 function todo2(){
