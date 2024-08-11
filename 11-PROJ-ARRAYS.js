65facb3629245256fc709b0e3fdce7ede5626c2c
DARKVIEWNERD <vincentnarvaez391@gmail.com>
2024-08-11 12:39:03 +0800

    still ongoing

--- 11-PROJ-ARRAYS.js
+++ 11-PROJ-ARRAYS.js
@@ -1,14 +1,17 @@
-const inputElement=[];
+
 
 function addtodo(){
+    const inputElement=[];
    const todoList= document.querySelector('.js-todo-name').value;
+   const todoDate= document.querySelector('.js-todo-date').value;
    
-   inputElement.push(todoList);
+   inputElement.push( `${todoList} ${todoDate}`);
+   console.log(inputElement.length);
    let htmlLists='';
    document.querySelector('.js-todo-name').value='';
 for(let count=0;count<inputElement.length;count++){
     
-    const value=inputElement[count];
+    const value=inputElement[count];// using variable inside the bracket well help access value inside an array
      const html= `<p>${value}</p>`;
      htmlLists+=html;
 
@@ -18,4 +21,5 @@ for(let count=0;count<inputElement.length;count++){
     
 
 }
+
 
